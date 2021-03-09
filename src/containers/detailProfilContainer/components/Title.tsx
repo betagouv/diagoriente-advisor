8b0a99ca296fd95788f9f9c67dd313a65bbf7f31
Mahdi Hellal <mahdihellal@gmail.com>
2021-03-09 16:06:31 +0100

    Profil Experience titles

--- src/containers/detailProfilContainer/components/Title.tsx
+++ src/containers/detailProfilContainer/components/Title.tsx
@@ -1,6 +1,4 @@
-import Tooltip from 'rc-tooltip';
 import style from './title.module.scss';
-import 'rc-tooltip/assets/bootstrap_white.css';
 
 interface Props {
   title: string;
@@ -9,14 +7,7 @@ interface Props {
 const Title = ({ title, date }: Props) => {
   return (
     <div className={style.container}>
-      <Tooltip
-        overlayClassName={style.tooltip}
-        placement="bottom"
-        overlay={title}
-        arrowContent={<div className="rc-tooltip-arrow-inner" />}
-      >
-        <div className={style.title}>{title}</div>
-      </Tooltip>
+      <div className={style.title}>{title}</div>
       <div className={style.text}>{date}</div>
     </div>
   );
